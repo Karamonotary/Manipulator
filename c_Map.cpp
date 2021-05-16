55c387530d977b3cc516a907e3d41670cbe93966
Karamonotary <57524980+Karamonotary@users.noreply.github.com>
2021-05-16 22:59:47 +0300

    Add files via upload

--- c_Map.cpp
+++ c_Map.cpp
@@ -3,32 +3,39 @@
 //
 
 #include "td_map.h"
+#include "manipulator.h"
+#include "barrier.h"
 
 extern "C"
 {
 Map* create_map(int size_x, int size_y, int basa_x, int basa_y)
 {
-    std::list<MapObject*>objects;
     return new Map(size_x, size_y, basa_x,basa_y);
 }
-MapObject* create_object(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force)
+MapObject* create_barrier(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force,  int mov)
 {
-    return new MapObject(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force)
+    return new Barrier(x, y, mass, leght, width, angle, point_force_x,  point_force_y,   force, mov);
 
 }
 
-void add_object(Map* mp,MapObject* obj)
+MapObject* create_component_manipulator(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force,  int number)
+{
+    return new Manipulator( x,  y, mass, leght, width,  angle, point_force_x,  point_force_y,   force,  number);
+
+}
+
+void add_new_object(Map* mp,MapObject* obj)
 {
     mp->add_object(obj);
 }
 
 void print_information(Map* mp)
-{   printf("Print inform\n")
+{   printf("Print inform\n");
     mp->print();
 }
 
 void show_map(Map* mp)
-{ printf("Show inform\n")
+{ printf("Show inform\n");
 
     mp->show();
 }
--- test3.cpp
+++ test3.cpp
@@ -5,9 +5,9 @@
 
 Map* create_map(int size_x, int size_y, int basa_x, int basa_y);
 
-MapObject* create_object(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force)
+MapObject* create_object(int x, int y, double mass, double leght, double width, double angle,int point_force_x,  int point_force_y, double  force);
 
-void add_object(Map* mp,MapObject* obj);
+void add_new_object(Map* mp,MapObject* obj);
 
 void print_information(Map* mp);
 
@@ -15,21 +15,26 @@ void show_map(Map* mp);
 
 void delete_map(Map* mp);
 
-void main(){
+int main(){
 Map* mp;
 MapObject* obj;
 
 mp = create_map(10,10,1,1);
-if(*mp) printf("Map is create \n");
-obj = create_object(5, 5, 10, 2, 2, 90,  6, 6,10, 1);
-if(*obj) printf("Object is create \n")
-mp->add_object(mp,obj);
+
+obj = create_barrier(5, 5, 10, 2, 2, 90,  6, 6,10, 1);
+
+add_new_object(mp,obj);
+
+    obj = create_component_manipulator(7, 7, 10, 2, 2, 90,  6, 6,10, 1);
+
+
+    add_new_object(mp,obj);
 
 print_information(mp);
 
 show_map(mp);
 
-delete_map();
+delete_map(mp);
 
-
+return 1;
 }
